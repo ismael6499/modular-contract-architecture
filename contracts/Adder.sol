1cc83adda88a5c84bdadc09d305b178a95ace609
Agustin <agustin.acosta@eestn1ee.edu.ar>
2025-11-24 01:57:20 -0300

    'use onlyAdmin modifier to prevent not authorized changes'

--- contracts/Adder.sol
+++ contracts/Adder.sol
@@ -5,14 +5,32 @@ import "./interfaces/IResult.sol";
 
 contract Adder{
     
-    address resultAddress;
+    address public resultAddress;
+    address public admin;
+    uint256 public fee;
 
-    constructor(address resultAddress_){
+    modifier onlyAdmin() {
+        if(msg.sender != admin) revert();
+        _;
+    }
+
+    constructor(address resultAddress_, address admin_){
         resultAddress = resultAddress_;
+        admin = admin_;
+        fee = 5;
     }
 
     function addition(uint256 num1_, uint256 num2_) external {
         uint256 result = num1_ + num2_;
         IResult(resultAddress).setResultado(result);
     }
+
+    function setFee(uint256 newFee_) external onlyAdmin {
+        fee = newFee_;
+    }
+
+    function setAdmin(address newAdmin_) external onlyAdmin {
+        admin = newAdmin_;
+    }
+
 }
